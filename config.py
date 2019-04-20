0adf0f3709fb74aad9d0884c765ac211eb0279c1
Denis Mulin <gudo.m@yandex.ru>
2019-04-20 19:44:18 +0400

    added objects

---
+++ config.py
@@ -0,0 +1,55 @@
+import colors
+
+screen_width = 500
+screen_height = 400
+background_image = 'images/background1.jpg'
+
+frame_rate = 40
+
+row_count = 3
+brick_width = 60
+brick_height = 20
+brick_color = colors.RED1
+offset_y = brick_height + 10
+
+ball_speed = 10
+ball_radius = 8
+ball_color = colors.INDIANRED1
+
+paddle_width = 110
+paddle_height = 50
+paddle_color = colors.DEEPSKYBLUE1
+paddle_speed = 6
+
+status_offset_y = 5
+
+text_color = colors.YELLOW1
+initial_lives = 3
+lives_right_offset = 85
+lives_offset = screen_width - lives_right_offset
+score_offset = 5
+
+font_name = 'Arial'
+font_size = 20
+
+effect_duration = 20
+
+sounds_effects = dict(
+    brick_hit='sound_effects/brick_hit.wav',
+    paddle_hit='sound_effects/paddle_hit.wav',
+    level_complete='sound_effects/level_complete.wav',
+)
+
+message_duration = 2
+
+button_text_color = colors.WHITE,
+button_normal_back_color = colors.INDIANRED1
+button_hover_back_color = colors.INDIANRED2
+button_pressed_back_color = colors.INDIANRED3
+
+menu_offset_x = 20
+menu_offset_y = 300
+menu_button_w = 80
+menu_button_h = 50
+
+pause_ball = 3 * frame_rate
---
+++ objects.py
@@ -0,0 +1,178 @@
+from pygame.rect import Rect
+from roundrects import aa_round_rect
+import pygame
+import config as c
+
+
+class GameObject:
+    def __init__(self, x, y, w, h, speed=(0, 0)):
+        self.bounds = Rect(x, y, w, h)
+        self.speed = speed
+
+    @property
+    def left(self):
+        return self.bounds.left
+
+    @property
+    def right(self):
+        return self.bounds.right
+
+    @property
+    def top(self):
+        return self.bounds.top
+
+    @property
+    def bottom(self):
+        return self.bounds.bottom
+
+    @property
+    def width(self):
+        return self.bounds.width
+
+    @property
+    def height(self):
+        return self.bounds.height
+
+    @property
+    def center(self):
+        return self.bounds.center
+
+    @property
+    def centerx(self):
+        return self.bounds.centerx
+
+    @property
+    def centery(self):
+        return self.bounds.centery
+
+    def draw(self, surface):
+        pass
+
+    def move(self, dx, dy):
+        self.bounds = self.bounds.move(dx, dy)
+
+    def update(self):
+        """"""
+        if self.speed == [0, 0]:
+            return
+
+        self.move(*self.speed)
+
+
+class Ball(GameObject):
+    def __init__(self, x, y, r, color, speed):
+        GameObject.__init__(self, x - r, y - r, r * 2, r * 2, speed)
+        self.radius = r
+        self.diameter = r * 2
+        self.color = color
+
+    def draw(self, surface):
+        r = pygame.draw.circle(surface, self.color, self.center, self.radius)
+
+    def update(self):
+        super().update()
+
+
+class Brick(GameObject):
+    def __init__(self, x, y, w, h, color, special_effect=None):
+        GameObject.__init__(self, x, y, w, h)
+        self.color = color
+        self.special_effect = special_effect
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.bounds)
+
+
+class Button(GameObject):
+    def __init__(self, x, y, w, h, text, on_click=lambda x: None, padding=0):
+        super().__init__(x, y, w, h)
+        self.state = 'normal'
+        self.on_click = on_click
+
+        self.text = TextObject(x + padding, y + padding, lambda: text, c.button_text_color, c.font_name, c.font_size)
+
+    @property
+    def back_color(self):
+        return dict(normal=c.button_normal_back_color,
+                    hover=c.button_hover_back_color,
+                    pressed=c.button_pressed_back_color)[self.state]
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.back_color, self.bounds)
+        self.text.draw(surface)
+
+    def handle_mouse_event(self, type, pos):
+        if type == pygame.MOUSEMOTION:
+            self.handle_mouse_move(pos)
+        elif type == pygame.MOUSEBUTTONDOWN:
+            self.handle_mouse_down(pos)
+        elif type == pygame.MOUSEBUTTONUP:
+            self.handle_mouse_up(pos)
+
+    def handle_mouse_move(self, pos):
+        if self.bounds.collidepoint(pos):
+            if self.state != 'pressed':
+                self.state = 'hover'
+        else:
+            self.state = 'normal'
+
+    def handle_mouse_down(self, pos):
+        if self.bounds.collidepoint(pos):
+            self.state = 'pressed'
+
+    def handle_mouse_up(self, pos):
+        if self.state == 'pressed':
+            self.on_click(self)
+            self.state = 'hover'
+
+
+class TextObject:
+    def __init__(self, x, y, text_func, color, font_name, font_size):
+        self.pos = (x, y)
+        self.text_func = text_func
+        self.color = color
+        self.font = pygame.font.SysFont(font_name, font_size)
+        self.bounds = self.get_surface(text_func())
+
+    def draw(self, surface, centralized=False):
+        text_surface, self.bounds = self.get_surface(self.text_func())
+        if centralized:
+            pos = (self.pos[0] - self.bounds.width // 2, self.pos[1])
+        else:
+            pos = self.pos
+        surface.blit(text_surface, pos)
+
+    def get_surface(self, text):
+        text_surface = self.font.render(text, False, self.color)
+        return text_surface, text_surface.get_rect()
+
+    def update(self):
+        pass
+
+
+class Paddle(GameObject):
+    def __init__(self, x, y, w, h, color, offset):
+        GameObject.__init__(self, x, y*1.25, w, h)
+        self.color = color
+        self.offset = offset
+        self.moving_left = False
+        self.moving_right = False
+
+    def draw(self, surface):
+        aa_round_rect(surface, self.bounds, self.color)
+
+    def handle(self, key):
+        if key == pygame.K_LEFT:
+            self.moving_left = not self.moving_left
+        else:
+            self.moving_right = not self.moving_right
+
+    def update(self):
+        if self.moving_left:
+            dx = -(min(self.offset, self.left))
+        elif self.moving_right:
+            dx = min(self.offset, c.screen_width - self.right)
+        else:
+            return
+
+        self.move(dx, 0)
